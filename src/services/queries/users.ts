431163a6b450ab94b647bc4dac0f203b0bf91678
IgorBraz <igorbraz81@outlook.com>
2023-01-06 19:52:32 -0300

    46.Adding Serialize

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -3,16 +3,21 @@ import { genId } from '$services/utils';
 import { client } from '$services/redis';
 import { usersKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => { };
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => { };
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
-	await client.hSet(usersKey(id), {
-		username: attrs.username,
-		password: attrs.password
-	});
+
+	await client.hSet(usersKey(id), serialize(attrs));
 
 	return id;
+};
+
+const serialize = (user: CreateUserAttrs) => {
+	return {
+		username: user.username,
+		password: user.password
+	}
 };
