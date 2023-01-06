c1fb9e2581d6de0711726a57fef2d51e26cbfb06
IgorBraz <igorbraz81@outlook.com>
2023-01-06 19:57:34 -0300

    47.Fetching a User

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -5,7 +5,11 @@ import { usersKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => { };
 
-export const getUserById = async (id: string) => { };
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(usersKey(id));
+
+	return deserialize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
@@ -21,3 +25,11 @@ const serialize = (user: CreateUserAttrs) => {
 		password: user.password
 	}
 };
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id: id,
+		username: user.username,
+		password: user.password
+	};
+}
